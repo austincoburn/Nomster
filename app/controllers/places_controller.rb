588ba0e85764b4dfdd90a43793bd9501ffdb1b0a
Austin Coburn <austincoburn41@gmail.com>
2019-08-22 01:21:35 +0000

    Set up maps integration

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
+		@places = Place.all
 		@place = current_user.places.create(place_params)
 		if @place.valid?
 			redirect_to root_path
